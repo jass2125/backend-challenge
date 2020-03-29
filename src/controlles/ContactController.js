e2e20a878606cb11f61a21778795921da97d0677
Anderson Souza <jair_anderson_bs@hotmail.com>
2020-03-29 19:51:25 -0300

    Adicionando o usuário que realizando a operação

--- src/controlles/ContactController.js
+++ src/controlles/ContactController.js
@@ -5,16 +5,15 @@ const User = require('../models/User');
 class ContactController {
 
   async findAll(req, res) {
-    const contacts = await Contact.findAll({ include: [{ model: User }] });
+    const contacts = await Contact.findAll({ where: { id_user: req.id }, include: [{ model: User }] });
     return res.json(contacts);
   }
 
   async delete(req, res) {
-    const { id } = req.params
-    const contact = await Contact.destroy({ where: { id } });
+    const { id } = req.params;
+    const contact = await Contact.destroy({ where: { id, id_user: req.id } });
     return res.json(contact);
   }
-
 
   async update(req, res) {
     const { id } = req.params;
@@ -28,7 +27,7 @@ class ContactController {
     }
     const con = req.body;
     con.id_user = req.id
-    
+
     const contactSaved = await Contact.update(con, {
       where: { id }
     }
