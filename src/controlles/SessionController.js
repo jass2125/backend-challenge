ad136437513b832c179213639acd08cfd9155d9d
Anderson Souza <jair_anderson_bs@hotmail.com>
2020-03-29 01:56:40 -0300

    Criando login de usuário

--- src/controlles/SessionController.js
+++ src/controlles/SessionController.js
@@ -1,9 +1,22 @@
+const User = require('../models/User');
+
 class SessionController {
 
   async login(req, res) {
 
     const { email, password } = req.body;
-    
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(403).json({ msg : 'The password is incorrect'});
+    }
+    if (user) {
+      const checkPassword = await user.checkPassword(password);
+      
+    }
+
+    console.log(email)
+    console.log(password)
+    return res.json({ sdfsd: 'sdsdf' });
 
   }
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,25 @@
 const { Model, Sequelize } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 class User extends Model {
 
-  static init(sequelize){
+  static init(sequelize) {
     super.init({
       email: Sequelize.STRING,
-      password_HASH: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
       password: Sequelize.VIRTUAL
     }, {
-      sequelize
+      sequelize,
+      tableName: 'users'
     })
+  }
+
+  async checkPassword(password) {
+    if (password) {
+      console.log(password);
+      
+      return await bcrypt.compare(password, this.password_hash);
+    }
   }
 
 }
